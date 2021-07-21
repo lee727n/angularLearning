73d663811413da26eaadedaf04770f95b666975f
Mikez_ios <lee727n@aliyun.com>
2021-07-21 16:40:45 +0800

    ngfor指令

---
+++ src/app/app.component.ts
@@ -0,0 +1,56 @@
+import { Component } from '@angular/core';
+
+interface TopMenu {
+  title: string;
+  link: string;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  selectedIndex = -1;
+  menus: TopMenu[] = [
+    {
+      title: '热门',
+      link: ''
+    },
+    {
+      title: '男装',
+      link: ''
+    },
+    {
+      title: '百货',
+      link: ''
+    },
+    {
+      title: '运动',
+      link: ''
+    },
+    {
+      title: '手机',
+      link: ''
+    },
+    {
+      title: '家纺',
+      link: ''
+    },
+    {
+      title: '食品',
+      link: ''
+    },
+    {
+      title: '电器',
+      link: ''
+    },
+
+  ];
+
+  handleclick(index) {
+    console.log('当前选中值为---------', index)
+    this.selectedIndex = index
+  }
+}
+
